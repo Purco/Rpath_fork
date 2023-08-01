b647c120cc7b46f397ef26a0b72f73c19bc99cf8
Purco <ralienpurco@gmail.com>
2023-08-01 16:47:27 +0200

    block 1

--- R/param.R
+++ R/param.R
@@ -175,9 +175,9 @@ check.rpath.params <- function(Rpath.params){
     warning('Model must contain at least 1 detrital group')
     w <- w + 1
   }
-  if(nrow(Rpath.params$model[Type == 3, ]) == 0){
-    warning('Model must contain at least 1 fleet')
-  }
+  # if(nrow(Rpath.params$model[Type == 3, ]) == 0){
+  #   warning('Model must contain at least 1 fleet')
+  # }
   
   #Check that there is the proper number of columns
   n.groups <- nrow(Rpath.params$model)
